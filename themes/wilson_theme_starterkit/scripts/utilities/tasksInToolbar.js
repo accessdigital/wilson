3e70076cf1ad65b617bec5cd1bf19cea19fa17db
Paul Gregory <paul.gregory@weareaccess.co.uk>
2021-01-31 04:01:53 +0000

    Inventing the tasksInToolbar to move the page tasks to the toolbar.

---
+++ themes/wilson_theme_starterkit/scripts/utilities/tasksInToolbar.js
@@ -0,0 +1,53 @@
+/**
+ * @file
+ * tasksInToolbar
+ *
+ * Moves the local task tabs from the page to the admin toolbar.
+ */
+(function (Drupal) {
+
+  Drupal.behaviors.tasksInToolbar = {
+    attach: function (context, settings) {
+      const editTabs = document.querySelectorAll('.local-tasks-block > ul');
+
+      editTabs.forEach((editTab) => {
+        const taskItems = editTab.querySelectorAll('li');
+        taskItems.forEach((taskItem) => {
+          taskItem.classList.add('menu-item');
+        });
+
+        const toolbarToolsInnerDiv = document.createElement('div');
+        toolbarToolsInnerDiv.classList.add('toolbar-menu-tasks');
+
+        const toolbarToolsNav = document.createElement('nav');
+        toolbarToolsNav.classList.add('toolbar-lining', 'clearfix');
+
+        const toolbarToolsDiv = document.createElement('div');
+        toolbarToolsDiv.classList.add('toolbar-tray', 'toolbar-tray-horizontal');
+        toolbarToolsDiv.setAttribute('id', 'toolbar-item-tasks-tray');
+        toolbarToolsDiv.setAttribute('data-toolbar-tray', 'toolbar-item-tasks-tray');
+
+        editTab.classList.add('toolbar-menu', 'claro-toolbar-menu');
+        toolbarToolsInnerDiv.appendChild(editTab);
+        toolbarToolsNav.appendChild(toolbarToolsInnerDiv);
+        toolbarToolsDiv.appendChild(toolbarToolsNav);
+
+        const toolbar = context.querySelector('#toolbar-bar');
+        const toolbarTab = document.createElement('div');
+        toolbarTab.classList.add('toolbar-tab');
+
+        const toolbarLink = document.createElement('a');
+        toolbarLink.classList.add('toolbar-icon', 'toolbar-icon-edit', 'trigger', 'toolbar-item');
+        toolbarLink.setAttribute('id', 'toolbar-item-tasks');
+        toolbarLink.setAttribute('data-toolbar-tray', 'toolbar-item-tasks-tray');
+        toolbarLink.setAttribute('role', 'button');
+        toolbarLink.innerHTML = 'Page Actions';
+
+        toolbarTab.appendChild(toolbarLink);
+        toolbarTab.appendChild(toolbarToolsDiv);
+        toolbar.prepend(toolbarTab);
+      });
+    }
+  };
+
+})(Drupal);
