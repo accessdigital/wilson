9a37deb2846500f45a368ed25b3dc5cee1c0369b
Paul Gregory <paul.gregory@weareaccess.co.uk>
2023-02-17 00:54:17 +0000

    Refactor JS and styling for the main menu. Reduces some complexity and fixes bugs associated with firing event handlers twice.

--- themes/wilson_theme_starterkit/src/js/components/main-menu.js
+++ themes/wilson_theme_starterkit/src/js/components/main-menu.js
@@ -2,7 +2,7 @@
  * @file
  * Main menu.
  */
-((Drupal) => {
+((Drupal, once) => {
   /**
    * Attaches the main menu behaviour.
    *
@@ -13,102 +13,13 @@
    */
   Drupal.behaviors.mainMenu = {
     attach(context) {
-      // Only progress if there is a menu element to work with.
-      if (context.querySelector(".navigation nav .menu")) {
-        const body = context.querySelector("body");
-        const nav = context.querySelector(".navigation nav");
-        const topLevelMenu = context.querySelector(".navigation .menu");
-        const topLevelSubmenuLinks = context.querySelectorAll(
-          ".navigation .menu > li.has-submenu > a, .navigation .menu > li.has-submenu > span"
-        );
-        const submenuLinks = context.querySelectorAll(
-          ".navigation li.has-submenu > a, .navigation li.has-submenu > span"
-        );
-        const backLinks = context.querySelectorAll(".back-link a");
+      const body = document.querySelector("body");
+      const nav = context.querySelector(".navigation nav");
+      const topLevelMenu = context.querySelector(".navigation .menu");
 
-        // Remove active state from active menu links.
-        const removeActiveLink = (activeLinks) => {
-          Array.prototype.forEach.call(activeLinks, (activeLink) => {
-            activeLink.classList.remove("menu-item-active");
-            if (
-              activeLink.hasAttribute("aria-expanded") &&
-              activeLink.getAttribute("aria-expanded") === "true"
-            ) {
-              activeLink.setAttribute("aria-expanded", "false");
-            }
-          });
-        };
-
-        // Setup event listeners for back links.
-        Array.prototype.forEach.call(backLinks, (backLink) => {
-          const parentWrapper = backLink.closest(".submenu-wrapper");
-          const parentMenuLink = parentWrapper.previousElementSibling;
-
-          backLink.addEventListener("click", (event) => {
-            event.preventDefault();
-            event.stopPropagation();
-
-            parentWrapper.classList.remove("menu-item-active");
-            parentMenuLink.setAttribute("aria-expanded", "false");
-          });
-        });
-
-        Array.prototype.forEach.call(submenuLinks, (link) => {
-          const siblingEl = link.nextElementSibling;
-          const parentEl = link.closest("ul");
-
-          /*
-           * Create event listener to set the submenu as active when the
-           * corresponding link is clicked.
-           */
-          link.addEventListener("click", (event) => {
-            event.preventDefault();
-
-            const activeMenuItems =
-              parentEl.querySelectorAll(".menu-item-active");
-            if (!link.classList.contains("menu-item-active")) {
-              // Remove active state from previously selected links.
-              if (activeMenuItems) {
-                removeActiveLink(activeMenuItems);
-              }
-              // Set the clicked link's submenu to active.
-              link.classList.add("menu-item-active");
-              link.setAttribute("aria-expanded", "true");
-              siblingEl.classList.add("menu-item-active");
-            } else {
-              link.classList.remove("menu-item-active");
-              link.setAttribute("aria-expanded", "false");
-              siblingEl.classList.remove("menu-item-active");
-            }
-          });
-        });
-
-        // Add event listener to top level items with submenus.
-        Array.prototype.forEach.call(topLevelSubmenuLinks, (link) => {
-          // Check for top submenus in the top level menu.
-          const topLevelMenuWrapper = link.nextElementSibling;
-          const submenus = topLevelMenuWrapper.querySelectorAll(
-            ".submenu-wrapper > .menu-level-2"
-          );
-
-          if (submenus.length === 0) {
-            topLevelMenuWrapper.firstElementChild.classList.add("no-submenus");
-          }
-
-          link.addEventListener("click", () => {
-            const activeSubmenus =
-              topLevelMenuWrapper.querySelectorAll(".menu-item-active");
-
-            // Set the child submenus to inactive.
-            if (activeSubmenus.length > 1) {
-              removeActiveLink(activeSubmenus);
-            }
-
-            body.classList.toggle("menu-active");
-          });
-        });
-
-        // Setup mobile header.
+      // Initialise the mobile menu. Populate nav with an additional header containing a menu close button.
+      // @TODO move mobile header in to template so it's easier override without editing this file.
+      once("mobileMenu", "body", context).forEach(() => {
         const menuHeader = document.createElement("div");
         const menuClose = document.createElement("button");
         const menuOpen = document.querySelector(".menu-open");
@@ -119,52 +30,109 @@
         menuHeader.prepend(menuClose);
         nav.prepend(menuHeader);
 
-        menuClose.addEventListener("click", () => {
-          const activeMenuItems =
-            document.querySelectorAll(".menu-item-active");
-          body.classList.remove("mobile-menu-is-active");
-          nav.classList.remove("menu-item-active");
-          topLevelMenu.classList.remove("menu-item-active");
-          menuOpen.setAttribute("aria-expanded", "false");
-
-          if (activeMenuItems) {
-            removeActiveLink(activeMenuItems);
-          }
-        });
-
-        // Create event listener for menu toggle icon.
+        // Menu open links - toggle mobile menu open.
         if (menuOpen) {
           menuOpen.addEventListener("click", () => {
-            const activeMenuItems =
-              document.querySelectorAll(".menu-item-active");
+            this.makeActive([body, nav, topLevelMenu]);
+            this.makeInactive(nav.querySelectorAll(".menu-item-active"));
+            menuOpen.setAttribute("aria-expanded", "true");
+
+            // Set keyboard focus on the first link of the opened menu.
             const firstLink = topLevelMenu.querySelector(
               ".menu-level-0 > li > a"
             );
-
-            body.classList.add("mobile-menu-is-active");
-            nav.classList.add("menu-item-active");
-            topLevelMenu.classList.add("menu-item-active");
-            menuOpen.setAttribute("aria-expanded", "true");
             firstLink.focus();
-
-            // Remove active class on any submenus when the menu is toggled.
-            if (activeMenuItems) {
-              removeActiveLink(activeMenuItems);
-            }
           });
         }
+
+        // Menu close links - toggle mobile menu closed.
+        menuClose.addEventListener("click", () => {
+          this.makeInactive([body, nav, topLevelMenu]);
+          this.makeInactive(nav.querySelectorAll(".menu-item-active"));
+          menuOpen.setAttribute("aria-expanded", "false");
+        });
+
+        // Close the mobile menu when moving up to lg breakpoint.
+        mediaQueryCheck(window.mediaQuery.lg, () => {
+          menuClose.click();
+        }, () => {});
 
         // Clear any active menus when clicking outside of the navigation.
         document.addEventListener("mouseup", (e) => {
           if (!nav.contains(e.target)) {
-            const activeMenuItems =
-              context.querySelectorAll(".menu-item-active");
-            if (activeMenuItems) {
-              removeActiveLink(activeMenuItems);
-            }
+            this.makeInactive(context.querySelectorAll(".menu-item-active"));
           }
         });
-      }
+      });
+
+      // Setup event listeners for the "go back" links shown in mobile menu.
+      once("backLinks", ".back-link a", context).forEach((backLink) => {
+        const parentWrapper = backLink.closest(".submenu-wrapper");
+        const parentMenuLink = parentWrapper.previousElementSibling;
+
+        backLink.addEventListener("click", (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          this.makeInactive([parentWrapper, parentMenuLink]);
+        });
+      });
+
+      // Setup event listener to toggle the submenu associated with clicking a menu link.
+      once("submenuLinks", ".navigation li.has-submenu > a, .navigation li.has-submenu > span", context).forEach((link) => {
+        const siblingEl = link.nextElementSibling;
+        const parentEl = link.closest("ul");
+
+        link.addEventListener("click", (event) => {
+          event.preventDefault();
+
+          if (!link.classList.contains("menu-item-active")) {
+            // Remove active state from previously selected links.
+            this.makeInactive(parentEl.querySelectorAll(".menu-item-active"));
+
+            this.makeActive([link, siblingEl]);
+          } else {
+            this.makeInactive([link, siblingEl]);
+          }
+        });
+      });
+    },
+
+    // Function to add the active state to an element.
+    makeActive(els) {
+      Array.prototype.forEach.call(els, (el) => {
+        if (el.tagName === 'BODY') {
+          el.classList.add("mobile-menu-is-active");
+        }
+        else {
+          el.classList.add("menu-item-active");
+          if (
+            el.tagName === "A" &&
+            el.hasAttribute("aria-expanded") &&
+            el.getAttribute("aria-expanded") === "false"
+          ) {
+            el.setAttribute("aria-expanded", "true");
+          }
+        }
+      });
+    },
+
+    // Function to remove the active state from an element.
+    makeInactive(els) {
+      Array.prototype.forEach.call(els, (el) => {
+        if (el.tagName === 'BODY') {
+          el.classList.remove("mobile-menu-is-active");
+        }
+        else {
+          el.classList.remove("menu-item-active");
+          if (
+            el.tagName === "A" &&
+            el.hasAttribute("aria-expanded") &&
+            el.getAttribute("aria-expanded") === "true"
+          ) {
+            el.setAttribute("aria-expanded", "false");
+          }
+        }
+      });
     },
   };
-})(Drupal);
+})(Drupal, once);
