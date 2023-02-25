036b353f24eb3bd1dead5db64cf206f0b63f7964
Paul Gregory <paul.gregory@weareaccess.co.uk>
2023-02-25 13:28:01 +0000

    Heavily improved version of the menu JS and CSS. Also moves mobile menu overlay below mobile header and introduces a single hamburger toggle button which transforms into a cross icon when active.

--- themes/wilson_theme_starterkit/src/js/components/main-menu.js
+++ themes/wilson_theme_starterkit/src/js/components/main-menu.js
@@ -8,6 +8,7 @@
   let nav;
   let menuOpen;
   let menuClose;
+  let mobileMenuIsOpen = false;
 
   /**
    * Attaches the main menu behaviour.
@@ -27,28 +28,41 @@
         menuOpen = document.querySelector(".menu-open");
         menuClose = nav.querySelector(".menu-close");
 
-        // Menu open links - toggle mobile menu open.
+        // Menu open links - toggle mobile menu overlay.
         if (menuOpen) {
           menuOpen.addEventListener("click", () => {
-            this.openMenuOverlay();
+            if (mobileMenuIsOpen) {
+              this.closeMenuOverlay();
+            }
+            else {
+              this.openMenuOverlay();
+            }
           });
         }
 
         // Menu close links - toggle mobile menu closed.
         if (menuClose) {
           menuClose.addEventListener("click", () => {
-            this.closeMenuOverlay();
+            if (mobileMenuIsOpen) {
+              this.closeMenuOverlay();
+            }
           });
         }
 
-        // Close the mobile menu when moving up to lg breakpoint.
-        mediaQueryCheck(window.mediaQuery.lg, () => {
-          this.closeMenuOverlay();
+        // Close the mobile menu when moving in to the 'lg' breakpoint.
+        // This uses the mediaQueryCheck() facility which fires an event when moving in
+        // and out of a given breakpoint. See media-query-check.js.
+        mediaQueryCheck(window.mediaQuery.md, () => {
+          if (mobileMenuIsOpen) {
+            this.closeMenuOverlay();
+          }
         }, () => {});
 
-        // Clear any active menus when clicking outside of the navigation.
+        // Clear any active menus when clicking anywhere outside of the navigation.
+        // We use closeMenuOverlay() for this, which is a multipurpose function that
+        // closes the mobile and desktop overlays.
         document.addEventListener("mouseup", (e) => {
-          if (!nav.contains(e.target)) {
+          if (!nav.contains(e.target) && !menuOpen.contains(e.target)) {
             this.closeMenuOverlay();
           }
         });
@@ -67,38 +81,42 @@
 
     // Function to open the mobile menu overlay.
     openMenuOverlay() {
+      mobileMenuIsOpen = true;
       const body = document.querySelector("body");
       const topLevelMenu = nav.querySelector(".menu");
-      this.makeActive([body, nav, topLevelMenu]);
-      this.makeInactive(nav.querySelectorAll(".menu-item-active"));
-      menuOpen.setAttribute("aria-expanded", "true");
+      this.makeActive([body, nav, topLevelMenu, menuOpen]);
+
+      // Reset to the top level of the menu by closing any previously opened submenus.
+      this.makeInactive(nav.querySelectorAll(".is-active"));
 
       // Set keyboard focus on the first link of the opened menu.
       const firstLink = topLevelMenu.querySelector(
         ".menu-level-0 > li > a"
       );
-      firstLink.focus();
+      if (firstLink) {
+        firstLink.focus();
+      }
     },
 
     // Function to close the mobile menu overlay.
     closeMenuOverlay() {
+      mobileMenuIsOpen = false;
       const body = document.querySelector("body");
       const topLevelMenu = nav.querySelector(".menu");
-      this.makeInactive([body, nav, topLevelMenu]);
-      this.makeInactive(nav.querySelectorAll(".menu-item-active"));
-      menuOpen.setAttribute("aria-expanded", "false");
+      this.makeInactive([body, nav, topLevelMenu, menuOpen]);
+      this.makeInactive(nav.querySelectorAll(".is-active"));
     },
 
     // Function to add the active state to an element.
     makeActive(els) {
       Array.prototype.forEach.call(els, (el) => {
         if (el.tagName === 'BODY') {
-          el.classList.add("mobile-menu-is-active");
+          el.classList.add("with-menu-overlay");
         }
         else {
-          el.classList.add("menu-item-active");
+          el.classList.add("is-active");
           if (
-            el.tagName === "A" &&
+            (el.tagName === "A" || el.tagName === "BUTTON") &&
             el.hasAttribute("aria-expanded") &&
             el.getAttribute("aria-expanded") === "false"
           ) {
@@ -112,12 +130,12 @@
     makeInactive(els) {
       Array.prototype.forEach.call(els, (el) => {
         if (el.tagName === 'BODY') {
-          el.classList.remove("mobile-menu-is-active");
+          el.classList.remove("with-menu-overlay");
         }
         else {
-          el.classList.remove("menu-item-active");
+          el.classList.remove("is-active");
           if (
-            el.tagName === "A" &&
+            (el.tagName === "A" || el.tagName === "BUTTON") &&
             el.hasAttribute("aria-expanded") &&
             el.getAttribute("aria-expanded") === "true"
           ) {
@@ -135,9 +153,9 @@
       link.addEventListener("click", (event) => {
         event.preventDefault();
 
-        if (!link.classList.contains("menu-item-active")) {
+        if (!link.classList.contains("is-active")) {
           // Remove active state from previously selected links.
-          this.makeInactive(parentEl.querySelectorAll(".menu-item-active"));
+          this.makeInactive(parentEl.querySelectorAll(".is-active"));
 
           this.makeActive([link, siblingEl]);
         } else {
